b70536645d720965f6391e0d19136bf00411466c
adekoder <adwumiogunbiyi@gmail.com>
2018-12-11 09:50:50 +0100

    Fix issue with the required method from the ew update

--- flask_validator/validators.py
+++ flask_validator/validators.py
@@ -2,10 +2,15 @@ class Validators():
     @staticmethod
     def required(request_data, validation_arg=None):
         error_msg = 'This field is required'
-        if request_data is None or len(request_data) == 0:
+
+        if request_data is None :
             return {'status': False, 'message': error_msg}
-        else:
-            return {'status': True}
+            
+        if not isinstance(request_data, int):
+            if len(request_data) == 0:
+                return {'status': False, 'message': error_msg}
+        
+        return {'status': True}
 
     @staticmethod
     def max(request_data, validator_arg):
